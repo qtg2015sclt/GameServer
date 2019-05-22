74a09062c41b99a2323d29d4afad5e50c8afadc8
qtg2015sclt <ymdq1113@163.com>
2019-05-22 21:37:50 +0800

    add new user register

--- dispatcher.py
+++ dispatcher.py
@@ -73,17 +73,60 @@ class LoginService(Service):
         password = msg["Password"]
         # print 'Login data: username = ', username, ", password = ", password
         # db host and port for test
+        userid_query = {"username": username, "password": password}
+        res = self.query_db(userid_query)
+        if res:
+            userid = res["userid"]
+            print "userid = ", userid
+        else:
+            print "No such user."
+
+    def handle_new_account_register(self, msg, who):
+        """Handle new account register."""
+        username = msg["UserName"]
+        password = msg["Password"]
+        username_query = {"username": username}
+        res = self.query_db(username_query)
+        if res:  # user name exist when there is a result
+            print "UserName has been used."
+            return
+        # insert new dict:
+        # TODO: generate userid
+        userid = None
+        user_dict = {
+            "username": username,
+            "password": password,
+            "userid": userid
+        }
+        res = self.insert_new_user_dict(user_dict)
+        if res:
+            print "Register new account."
+        else:
+            print "Register failed."
+
+    def insert_new_user_dict(self, new_dict):
+        """Insert new account dict."""
         client = pymongo.MongoClient("mongodb://localhost:27017/")
         db = client["tpsdemo"]
         col = db["LocalAuth"]
-        myquery = {"username": username, "password": password}
         try:
-            res = col.find(myquery).limit(1)[0]
+            res = col.insert_one(new_dict)
         except:
-            print "No such user"
-            raise Exception("No such user")
-        userid = res["userid"]
-        print "userid = ", userid
+            print "Insert new user failed."
+            res = None
+        return res
+
+    def query_db(self, query):
+        """Query db."""
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["tpsdemo"]
+        col = db["LocalAuth"]
+        try:
+            res = col.find(query).limit(1)[0]
+        except:
+            print "Query failed."
+            res = None
+        return res
 
 
 class GameSyncService(Service):
